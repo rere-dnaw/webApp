f950b486bea3aa07b51532e2b08bb5017d49f97e
rere-dnaw-WPC <rafal.wilk92@gmail.com>
2020-10-20 16:26:19 +0100

    separate style.css

--- app.py
+++ app.py
@@ -2,13 +2,9 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-posts = []
-
-
 @app.route('/')
 def homepage():
     return render_template('test.html')
-
 
 if __name__ == '__main__':
     app.run()
